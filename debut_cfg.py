e6beafc69a14c1dd4c5a1df97d6749eaee18a760
Louise726 <louisemarc726@yahoo.com>
2024-03-22 17:19:58 +0100

    partie vrf dans le début de la config

--- debut_cfg.py
+++ debut_cfg.py
@@ -7,9 +7,11 @@ def creation_texte_debut(router, ip_version, file):
     ecriture_fichier(file, "!\nversion 15.2\nservice timestamps debug datetime msec\nservice timestamps log datetime msec\n!\n")
     ecriture_fichier(file, "hostname "+ router.hostname + "\n!\n")
 
-    ecriture_fichier(file, "boot-start-marker\nboot-end-marker\n" + "!\n"*3)
+    ecriture_fichier(file, "boot-start-marker\nboot-end-marker\n" + "!\n"*2)
 
-    ecriture_fichier(file,"no aaa new-model\nno ip icmp rate-limit unreachable\nip cef\n" + "!\n"*6)
+    vrf_definition(router,file)
+
+    ecriture_fichier(file,"!\nno aaa new-model\nno ip icmp rate-limit unreachable\nip cef\n" + "!\n"*6)
 
     if ip_version == 6 :
         ecriture_fichier(file, "no ip domain lookup\nipv6 unicast-routing\nipv6 cef\n")
@@ -24,3 +26,13 @@ def creation_texte_debut(router, ip_version, file):
 
 def ecriture_fichier(file,text):
     file.write(text)
+
+def vrf_definition (router,file):
+    for i in router.interfaces :
+        if "VPN" in i.protocols :
+            ecriture_fichier(file, "vrf definition Client_" + i.client[0] +"\n")
+            ecriture_fichier(file, " rd " + i.client[1] +"\n")
+            ecriture_fichier(file, " route-target export " + i.client[2] +"\n")
+            ecriture_fichier(file, " route-target import " + i.client[2] +"\n !\n")
+            ecriture_fichier(file, " address-family ipv4\n")
+            ecriture_fichier(file, " exit-address-family\n!\n")
